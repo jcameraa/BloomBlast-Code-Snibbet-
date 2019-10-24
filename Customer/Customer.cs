dbeb26e03174f23d6f43629d78b843267ccf5c32
jcameraa <ahrtxv@gmail.com>
2019-10-24 13:17:22 -0400

    Code Snibbet

---
+++ Customer/Customer.cs
@@ -0,0 +1,238 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public enum Difficulty
+{
+    Custom,
+    Easy,
+    Medium,
+    Hard
+}
+
+/// <summary>
+/// The manager for each individul customer that appears. This script handles
+/// each customer indivisually.
+/// </summary>
+public class Customer : MonoBehaviour
+{
+    /// <summary>
+    /// The index of the customer in the customer manager list
+    /// </summary>
+    public int customerIndex;
+    [Space]
+
+    #region Variable
+    /// <summary>
+    /// The type of plant the customer wants
+    /// </summary>
+    public Dot DotWanted;
+
+    /// <summary>
+    /// The amount of plants this customer wants
+    /// </summary>
+    public float NumWanted;
+
+    // The animatior attached to the current
+    // character shown 
+    public Animator charAnim;
+
+    // The string of what type of plant
+    // the charcater wants, which is assigned
+    // from the tags 
+    public string charWanted;
+
+    public Difficulty LevelDifficulty;
+
+    /// <summary>
+    /// The visual representation of the NumWanted
+    /// </summary>
+    public Text HowManyWanted; 
+
+    // Used to initialize the types of plants and
+    // characters that will be shown on screen. 
+    [SerializeField]
+    public List<Image> _plantList;
+
+    public List<GameObject> _characterList;
+
+    protected Dictionary<string, GameObject> _plants;
+
+    protected Dictionary<string, GameObject> _character;
+
+    float rate = .5f;
+    #endregion
+
+    #region Difficulty Varables
+
+    private int MinWanted;
+    private int MaxWanted;
+
+    /// <summary>
+    /// Used to set up a Customer. This will give the ranges of numbers
+    /// each character might be asking for based on the difficulty we select, 
+    /// and then those numbers are sent to be randomly chosen for each customer
+    /// </summary>
+    /// <param name="difficulty">The difficultly chosen</param>
+    /// <param name="dotWanted">The type of Dot the customer is asking for</param>
+    /// <param name="minW">The minium amut of the Dot the customer could want</param>
+    /// <param name="maxW">The maxium amount of the Dot the cusotmer could want</param>
+    public void SetupCustomer(Difficulty difficulty, Dot dotWanted, int minW = 0, int maxW = 0)
+    {
+        DotWanted = dotWanted;
+        switch (difficulty)
+        {
+            case Difficulty.Easy:
+
+                MinWanted = 5;
+                MaxWanted = 10;
+                break;
+            case Difficulty.Medium:
+
+                MinWanted = 10;
+                MaxWanted = 15;
+                break;
+            case Difficulty.Hard:
+
+                MinWanted = 15;
+                MaxWanted = 20;
+                break;
+
+            case Difficulty.Custom:
+
+                MinWanted = minW;
+                MaxWanted = maxW;
+                break;
+            default:
+                break;
+        }
+
+        NumWanted = Random.Range(MinWanted, MaxWanted + 1);
+
+        _plants = new Dictionary<string, GameObject>(_plantList.Count);
+        foreach (var plant in _plantList)
+        {
+            plant.gameObject.SetActive(false);
+            _plants.Add(plant.tag, plant.gameObject);
+        }
+
+        _character = new Dictionary<string, GameObject>(_characterList.Count);
+        foreach (var chara in _characterList)
+        {
+            chara.gameObject.SetActive(false);
+            _character.Add(chara.tag, chara.gameObject);
+        }
+
+        HowManyWanted.text = NumWanted.ToString() + "x";
+
+    }
+    #endregion
+
+    #region Main Class Methods
+
+    /// <summary>
+    /// Displays the current plant the customer wants
+    /// </summary>
+    public void ShowPlant()
+    {
+        foreach (var plant in _plants)
+        {
+            plant.Value.SetActive(false);
+        }
+        var dotWanted = DotWanted.tag;
+        _plants[dotWanted].SetActive(true);
+        _plants[dotWanted].gameObject.transform.parent.gameObject.SetActive(false);
+        _plants[dotWanted].gameObject.transform.parent.gameObject.SetActive(true);
+    }
+
+    
+
+    /// <summary>
+    /// Shows the customer based on what is wanted.
+    /// Used with dev cheats. 
+    /// </summary>
+    /// <param name="charTag">The tag of the current customer</param>
+    public void choseCharacter(string charTag)
+    {
+
+        Debug.Log("HOw many times :0");
+        foreach (var chara in _character)
+        {
+            chara.Value.SetActive(false);
+        }
+        foreach (var plant in _plants)
+        {
+            plant.Value.SetActive(false);
+        }
+        charWanted = charTag;
+        _character[charWanted].SetActive(true);
+        charAnim = _character[charWanted].GetComponent<Animator>();
+        var dotWanted = charTag;
+        _plants[dotWanted].SetActive(true);
+    }
+
+
+    /// <summary>
+    /// Checks if the dot destoyed is what the customer wants
+    /// and if it has completed their order
+    /// </summary>
+    /// <param name="dot">The dot destroyed</param>
+    /// <param name="numDestroyed">THe amount destroyes</param>
+    /// <returns></returns>
+    public bool CheckDot(Dot dot, int numDestroyed)
+    {
+        Debug.Log("Checking for " + DotWanted.tag);
+        if (dot.gameObject.tag == DotWanted.tag)
+        {
+            _plants[DotWanted.tag].gameObject.GetComponentInParent<Animator>().SetTrigger("Blip");
+            var before = NumWanted;
+            Debug.Log("Found " + DotWanted.tag);
+            NumWanted --;
+
+            //Tweener xTween = DOTween.To(x => before = x, before, NumWanted,1f);
+            //xTween.onUpdate += delegate { HowManyWanted.text = before.ToString("F0") + "x"; };
+            HowManyWanted.text = NumWanted.ToString() + "x";
+
+            Debug.Log(before + "  this is what before is" + NumWanted + "   this is what after is");
+        }
+        else
+        {
+            AnalyticsManager.Instance.LogNonObjectiveMatch(numDestroyed);
+        }
+        if (NumWanted <= 0)
+        {
+            charAnim = _character[charWanted].GetComponentInChildren<Animator>();
+            ////// PUT IT HERE ////////
+            return true;
+        }
+        //HowManyWanted.text = NumWanted.ToString() + "x";
+        return false;
+
+    }
+
+
+    /// <summary>
+    /// Displays the customer associated with the 
+    /// plant the customer wants
+    /// </summary>
+    public void ShowCharacter()
+    {
+
+
+       foreach (var chara in _character)
+        {
+            chara.Value.SetActive(false);
+        }
+
+        HowManyWanted.text = NumWanted.ToString() + "x";
+        charWanted = DotWanted.tag;
+        _character[charWanted].SetActive(true);
+        charAnim = _character[charWanted].GetComponentInChildren<Animator>();
+
+        HowManyWanted.text = NumWanted.ToString() + "x";
+        Debug.Log("Is this where that happens");
+    }
+    #endregion
+}
---
+++ Customer/CustomerManager.cs
@@ -0,0 +1,160 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// enum that stores the current state the customer is in.
+/// Currently being used to handle animation states.
+/// </summary>
+public enum CustomerState
+{
+    Normal,
+    Nervous
+}
+
+public class CustomerManager : MonoBehaviour
+{
+    #region Variables
+
+    public CustomerState CurrentState;
+
+    // How many dot was in the current match
+    public int NumDestoryed;
+
+    public Customer[] customerList;
+
+    private LevelManager levelMang; 
+
+
+    [HideInInspector] public bool sameCustomer;
+
+    #endregion
+
+    #region Initialization
+    public void Init(int numOfCustomers, int moveLimit)
+    {
+        levelMang = GetComponentInParent<LevelManager>();
+        var dotTypes = levelMang.board.GetComponent<BoardManager>().dotTypes;
+        foreach (var customer in customerList)
+        {
+            customer.SetupCustomer(Difficulty.Easy,
+            dotTypes[Random.Range(0, dotTypes.Length)]);
+            customer.ShowPlant();
+            customer.ShowCharacter();
+            CheckIfSame(customerList);
+        }
+
+      //  AudioManager.Instance.GetFreeSource().PlayOneShot(happyCustomerSound);
+    }
+    #endregion
+
+    #region Main Class Methods
+    /// <summary>
+    /// Checks if the group of dots that are destoryed
+    /// match any of the customers currently on screen
+    /// </summary>
+    /// <param name="dotDestroyed"> One of the dots in the current match</param>
+    /// <param name="numDestroyed"> The length of the current match </param>
+    public void CheckDot(Customer[] listOfCust, Dot dotDestroyed, int numDestroyed)
+    {
+        //AudioManager.Instance.GetFreeSource().PlayOneShot(clickSound);
+        Debug.Log("Same Customer: " + sameCustomer);
+        if (!sameCustomer)
+        {
+            Debug.Log("Same Customer should equal false.");
+            for (int i = 0; i < listOfCust.Length; i++)
+            {
+                if (listOfCust[i].CheckDot(dotDestroyed, numDestroyed))
+                {
+                    RemoveCustomer(listOfCust[i]);
+                }
+            }
+        }
+        else
+        {
+            Debug.Log("Same Customer should equal true.");
+            if (listOfCust[0].CheckDot(dotDestroyed, numDestroyed))
+            {
+                RemoveCustomer(listOfCust[0]);
+            }
+        }  
+    }
+
+    /// <summary>
+    /// Removes the customer whose order is complete.
+    /// </summary>
+    /// <param name="customer">The customer to remove</param>
+    private void RemoveCustomer(Customer customer)
+    {
+        levelMang.audMang.GetComponentInChildren<UISounds>().Play("HappyCusomer");
+        var dotTypes = levelMang.board.GetComponent<BoardManager>().dotTypes;
+        customer.SetupCustomer(
+           Difficulty.Easy, dotTypes[Random.Range(0, dotTypes.Length)]);
+        customer.ShowPlant();
+        customer.ShowCharacter();
+        CheckIfSame(customerList);
+        levelMang.numCustomers--;
+      //  AudioManager.Instance.GetFreeSource().PlayOneShot(happyCustomerSound);
+    }
+
+    /// <summary>
+    /// Checks if the two active customers are the same.
+    /// ONLY WORKS WHEN <c>customerList</c> HAS TWO CUSTOMERS
+    /// </summary>
+    /// <param name="listOfCust">The list of active customers.</param>
+    public void CheckIfSame(Customer[] listOfCust)
+    {
+        if (listOfCust.Length >= 2){
+            Debug.Log("Customer 1: " + listOfCust[0].charWanted);
+            Debug.Log("Customer 2: " + listOfCust[1].charWanted);
+            sameCustomer = listOfCust[0].charWanted.Equals(listOfCust[1].charWanted);
+            Debug.Log("Check if same: " + sameCustomer);
+        }
+    }
+
+    /// <summary>
+    /// Uses <see cref="CurrentState"/> to determine which
+    /// animations the customers will use.
+    /// </summary>
+    public void CheckAnimationState()
+    {
+        SetCustomerState();
+
+        switch (CurrentState)
+        {
+            case CustomerState.Nervous:
+                Debug.Log("ZOMG IM SO NERVOUS DAD");
+                break;
+            case CustomerState.Normal:
+                Debug.Log("Hi, how are ya?");
+                break;
+            default:
+                Debug.Log("00111100 00001111 11100001");
+                break;
+        }
+    }
+
+    /// <summary>
+    /// Uses <see cref="IsNervous"/> to determine what the value
+    /// of <see cref="CurrentState"/> is going to be.
+    /// </summary>
+    public void SetCustomerState()
+    {
+        CurrentState = IsNervous() ? CustomerState.Nervous : CustomerState.Normal;
+    }
+    #endregion
+
+    #region ScoreManager Getters
+    /// <summary>
+    /// Gets the value of <see cref="ScoreManager.RemainingMoves"/>
+    /// and checks if the value is less than or equal to 5.
+    /// </summary>
+    /// <returns> Whether or not there are 5 or less moves remaining.</returns>
+    public bool IsNervous()
+    {
+
+        return levelMang.moveLimit <= 5;
+    }
+    #endregion
+}
